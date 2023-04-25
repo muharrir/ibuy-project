a2cce9335e3be438545ed00b5d2537afb3ca47b2
Muharrir <muharrir97@gmail.com>
2023-04-25 20:28:33 +0700

    initial commit

---
+++ src/pages/_document.js
@@ -0,0 +1,22 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
